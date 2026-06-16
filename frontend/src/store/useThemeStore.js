470fec4ddfc341ff223e24e59beafdcba7700e0c
Aryan9059 <iec2024031@iiita.ac.in>
2026-06-16 19:32:31 +0530

    Revert "Committed the backend and frontend."
    This reverts commit 33d6893dbad38e5650c58322525b1c27a2615a2b.

--- frontend/src/store/useThemeStore.js
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-
-export const useThemeStore = create((set) => ({
-  theme: localStorage.getItem("streamify-theme") || "luxury",
-  setTheme: (theme) => {
-    localStorage.setItem("streamify-theme", theme);
-    set({ theme });
-  },
-}));
